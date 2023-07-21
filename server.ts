9df76a5ed5edd8b5c4655ec804722f6dd79654c7
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-21 17:49:07 +0500

    Added room route

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import { v4 } from "uuid";
 
 const app = express();
 
@@ -9,5 +10,13 @@ const io = require("socket.io")(server);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${v4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
 server.listen(3000);
